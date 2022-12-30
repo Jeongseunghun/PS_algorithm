ff3f29081806b588717a171fe78bc5abed9c7b4d
sh Jeong <seunghun311@naver.com>
2022-12-31 04:57:16 +0900

    221231 solved 0problem

--- implementation/[re]12933 오리.py
+++ implementation/[re]12933 오리.py
@@ -1,9 +1,27 @@
-duck = list(input())
+n = input()
+duck = n.split('k')
+
 cry = ['q','u','a','c','k']
-cnt = 0
+cnt = [0,0,0,0,0]
+res = []
 
-for i in duck:
-    if i =="q":
-            cnt+=1
+if len(n) % 5 !=0 or n[0] != "q":
+    print(-1)
+    exit()
 
-print(cnt)
+for i in range(len(duck)):
+    res.append(duck[i].count("q"))
+
+for i in n:
+    for j in range(5):
+        if i == cry[j]:
+            cnt[j] +=1
+            for k in range(5):
+                for l in range(k+1,5):
+                    if cnt[k] < cnt[l]:
+                    
+                        print(-1)
+                        exit()
+
+
+print(max(res))
--- implementation/tempCodeRunnerFile.py
+++ implementation/tempCodeRunnerFile.py
@@ -1,2 +1,29 @@
- # if ktx[command[1]-1][0] ==1:
-        #     ktx[command[1]-1][0] = 0
+n = input()
+duck = n.split('k')
+
+cry = ['q','u','a','c','k']
+cnt = [0,0,0,0,0]
+res = []
+
+if len(n) % 5 !=0 or n[0] != "q":
+    print(-1)
+    exit()
+
+for i in range(len(duck)):
+    res.append(duck[i].count("q"))
+
+for i in n:
+    for j in range(len(cry)):
+        if i == cry[j]:
+            cnt[j] +=1
+        for k in range(len(cnt)):
+            for l in range(k,len(cnt)):
+                if cnt[k] < cnt[l]:
+                    print(-1)
+                    exit()
+
+
+            
+
+
+print(max(res))
