98cd6ffe70e5edcad96ec4491938296c1cbeb47b
sh Jeong <seunghun311@naver.com>
2023-01-01 23:39:22 +0900

    230101 sovled 1problem

--- implementation/14719 빗물.py
+++ implementation/14719 빗물.py
@@ -1,1 +1,16 @@
 H,W = map(int,input().split())
+lst = list(map(int,input().split()))
+res = 0
+
+for i in range(1,W-1):
+    start = max(lst[:i])
+    end = max(lst[i+1:])
+    
+    short = min(start,end)
+    
+    if lst[i] < short:
+        res += short - lst[i]
+
+print(res)
+        
+            
