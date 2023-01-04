811042abcbcdb73ad851a4df5a8896859a378b3d
Jeongseunghun <seunghun311@naver.com>
2023-01-05 00:01:42 +0900

    230104 solved 3problem

--- Graph Traversal	/2667 단지번호붙이기.py
+++ Graph Traversal	/2667 단지번호붙이기.py
@@ -3,29 +3,37 @@ from collections import deque
 N= int(input())
 loc = [list(map(int,input())) for _ in range(N)]
 
-home = []
-cnt=0
 
-def bfs():
+
+def bfs(loc,x,y):
     #상하좌우
     dx=[-1,1,0,0]
     dy=[0,0,-1,1]
-    q= deque([(0,0)])
+    q= deque()
+    q.append((x,y))
+    loc[x][y] = 0
+    cnt=1
 
     while q:
         x,y = q.popleft()
         for i in range(4):
             nx = x+dx[i]
             ny = y+dy[i]
-            if 0<= nx < N and 0 <= ny < N:
-                if loc[nx][ny] == 1:
-                    loc[nx][ny] = loc[x][y] + 1
-                    q.append((nx,ny))
-    home.append(loc[nx-1][ny-1])    
-    cnt+=1
-            
-print(cnt)
-for i in home:
+            if 0> nx or nx >= N or 0 > ny or ny >= N:
+                continue
+                
+            if loc[nx][ny] == 1:
+                loc[nx][ny] = 0
+                q.append((nx,ny))
+                cnt+=1
+    return cnt
+
+
+count = [bfs(loc,i,j) for i in range(N) for j in range(N) if loc[i][j] == 1]
+count.sort()
+print(len(count))
+
+for i in count:
     print(i)
                 
     
