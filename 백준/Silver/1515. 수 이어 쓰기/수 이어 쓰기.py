6641970c8ccdd5c5d5ee0ba0434eda76db659bc4
yyaho <76212667+Jeongseunghun@users.noreply.github.com>
2023-06-17 22:50:00 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -1,12 +1,13 @@
-nums = input()
+n = input()
+
 i = 0
 while True:
-    i += 1
-    num = str(i)
-    while len(num) > 0 and len(nums) > 0:
-        if num[0] == nums[0]:
-            nums = nums[1:]
+    i+=1
+    num =str(i)
+    while len(num) > 0 and len(n) > 0:
+        if num[0] == n[0]:
+            n = n[1:]
         num = num[1:]
-    if nums == '':
+    if n == '':
         print(i)
         break
