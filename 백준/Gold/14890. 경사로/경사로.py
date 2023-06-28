2dfca6595961301865660479343449c602a62791
Hoonies <seunghun311@naver.com>
2023-06-28 11:49:46 +0900

    [Gold III] Title: 경사로, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -1,35 +1,40 @@
-n, l = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-ans = 0
+N,L = map(int,input().split())
+street = [list(map(int,input().split())) for _ in range(N)]
+cnt = 0
 
-
-def check_line(line):
-    for i in range(1, n):
-        if abs(line[i] - line[i - 1]) > 1:
+def check(line):
+    for i in range(1,N):
+        #1보다 크면 패스
+        if abs(line[i] - line[i-1]) > 1:
             return False
-        if line[i] < line[i - 1]:
-            for j in range(l):
-                if i + j >= n or line[i] != line[i + j] or slope[i + j]:
+        #앞이 클때
+        if line[i] < line[i-1]:
+            for j in range(L):
+                if i+j >= N or line[i] != line[i+j] or slope[i+j]:
                     return False
-                if line[i] == line[i + j]:
-                    slope[i + j] = True
-        elif line[i] > line[i - 1]:
-            for j in range(l):
-                if i - j - 1 < 0 or line[i - 1] != line[i - j - 1] or slope[i - j - 1]:
+                if line[i] == line[i+j]:
+                    slope[i+j] = True
+        #뒤가 클때
+        elif line[i] > line[i-1]:
+            for j in range(L):
+                if i-j-1<0 or line[i-1] != line[i-j-1] or slope[i-j-1]:
                     return False
-                if line[i - 1] == line[i - j - 1]:
-                    slope[i - j - 1] = True
+                if line[i-1] == line[i-j-1]:
+                    slope[i-j-1] = True
     return True
+                
 
+for i in range(N):
+    slope = [False] * N
+    if check([street[i][j] for j in range(N)]):
+        cnt += 1
 
-for i in range(n):
-    slope = [False] * n
-    if check_line([graph[i][j] for j in range(n)]):
-        ans += 1
+for j in range(N):
+    slope = [False] * N
+    if check([street[i][j] for i in range(N)]):
+        cnt += 1
 
-for j in range(n):
-    slope = [False] * n
-    if check_line([graph[i][j] for i in range(n)]):
-        ans += 1
-
-print(ans)
+print(cnt)
+        
+        
+    
