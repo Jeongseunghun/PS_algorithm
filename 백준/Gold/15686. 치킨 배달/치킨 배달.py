ed64b51b46e263b21d9e19f3a72682cb1b8d1558
Hoonies <seunghun311@naver.com>
2023-07-17 09:56:23 +0900

    [Gold V] Title: 치킨 배달, Time: 708 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+
+N,M = map(int,input().split())
+city = [list(map(int,input().split())) for _ in range(N)]
+
+chicken = []
+house = []
+
+for i in range(N):
+    for j in range(N):
+        if city[i][j] == 2:
+            chicken.append([i,j])
+        if city[i][j] == 1:
+            house.append([i,j])
+
+res = 999999
+
+for c in combinations(chicken,M):
+    tmp = 0
+    for i in house:
+        min_d = 999999
+        for j in range(len(c)):
+            min_d = min(min_d, abs(c[j][0]-i[0]) + abs(c[j][1]-i[1]))
+        tmp += min_d
+    res = min(res,tmp)
+
+print(res)          
+            
