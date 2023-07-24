64528e4ba495e4e40c657ed1c79715db4412d51c
Hoonies <seunghun311@naver.com>
2023-07-24 23:57:26 +0900

    [Silver I] Title: 영역 구하기, Time: 72 ms, Memory: 34200 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,26 +1,30 @@
 from collections import deque
+
 M,N,K = map(int,input().split())
+dist = [list(map(int,input().split())) for _ in range(K)]
 
-paper = [[0] * N for _ in range(M)]
-
-#칠하기
-for i in range(K):
-    sx,sy,ex,ey = map(int,input().split())
-    for i in range(sx,ex):
-        for j in range(M-sy-1,M-ey-1,-1):
-            paper[j][i] = 1
-    
-    
 #상하좌우
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
+paper = [[0 for _ in range(N)] for _ in range(M)]
+
+for i in range(K):
+    wid = dist[i][2]-dist[i][0]
+    hei = dist[i][3]-dist[i][1]
+    for k in range(dist[i][0],dist[i][0]+wid):
+        for j in range(dist[i][1],dist[i][1]+hei):
+            paper[j][k] = 1
+
+            
+
+tmp = []
 
 def bfs(x,y):
-    cnt = 1
     q = deque()
     q.append((x,y))
     paper[x][y] = 1
+    cnt = 1
     while q:
         x,y = q.popleft()
         for i in range(4):
@@ -30,18 +34,15 @@ def bfs(x,y):
                 if paper[nx][ny] == 0:
                     paper[nx][ny] = 1
                     q.append((nx,ny))
-                    cnt+=1
+                    cnt += 1
+        
     tmp.append(cnt)
     
-tmp = []
 for i in range(M):
     for j in range(N):
         if paper[i][j] == 0:
             bfs(i,j)
 
-tmp.sort()
+tmp.sort()     
 print(len(tmp))
-for i in tmp:
-    print(i,end = " ")
-
-
+print(*tmp)
