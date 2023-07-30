1e169a09bac6868fdcd3fde5c7c085784948bfe4
Hoonies <seunghun311@naver.com>
2023-07-30 16:18:49 +0900

    [Platinum III] Title: 달리기, Time: 632 ms, Memory: 197308 KB -BaekjoonHub

--- 백준/Platinum/16930. 달리기/달리기.py
+++ 백준/Platinum/16930. 달리기/달리기.py
@@ -1,29 +1,39 @@
 from collections import deque
 
-n,m,k=map(int,input().split())
-dx,dy=[1,-1,0,0],[0,0,1,-1]
-vis=[[-1]*m for _ in range(n)]
-a=[list(input()) for _ in range(n)]
-x1,y1,x2,y2=map(int,input().split())
+N,M,K = map(int,input().split())
+gym = [list(input()) for _ in range(N)]
+visited = [[-1]*M for _ in range(N)]
 
-q=deque()
-q.append((x1-1,y1-1))
-vis[x1-1][y1-1]=0
-while q:
-    y,x=q.popleft()
-    if y==x2-1 and x==y2-1:
-        print(vis[y][x])
-        exit()
-    for i in range(4):
-        for num in range(1,k+1):
-            xx=x+dx[i]*num
-            yy=y+dy[i]*num
-            if xx<0 or xx>=m or yy<0 or yy>=n: break
-            if a[yy][xx]=='#':break
-            if vis[yy][xx] == -1:
-                q.append((yy,xx))
-                vis[yy][xx]=vis[y][x]+1
-            elif vis[yy][xx]==vis[y][x]+1: continue #일단 쭉 가본다
-            else: break # 더 큰길은 탐색하지 않는다
+x1,y1,x2,y2 = map(int,input().split())
 
-print(-1)
+#상하좌우
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs():
+    q = deque()
+    q.append((x1-1,y1-1))
+    visited[x1-1][y1-1] = 0
+    while q:
+        x,y = q.popleft()
+        if x == x2-1 and y == y2-1:
+            return print(visited[x][y])
+        for i in range(4):
+            for j in range(1,K+1):
+                nx = x + dx[i] * j
+                ny = y + dy[i] * j
+                if 0 > nx or nx>=N or 0 > ny or ny >= M:
+                    break
+                if gym[nx][ny] == "#":
+                    break
+                if visited[nx][ny] == -1:
+                    q.append((nx,ny))
+                    visited[nx][ny] = visited[x][y] + 1
+                elif visited[nx][ny] == visited[x][y] + 1:
+                    continue
+                else:
+                    break
+    return print(-1)
+
+bfs()
+            
