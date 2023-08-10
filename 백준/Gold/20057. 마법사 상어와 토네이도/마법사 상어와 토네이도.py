35d2014aa88b62fd136beed2253c34584c539958
Hoonies <seunghun311@naver.com>
2023-08-11 02:06:33 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 1100 ms, Memory: 41312 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -1,57 +1,54 @@
 N = int(input())
-board = [list(map(int,input().split())) for _ in range(N)]
-#토네이도 시작
-sx,sy = N//2,N//2
+sand = [list(map(int,input().split())) for _ in range(N)]
+sx,sy= N//2, N//2
 
+#방향별 비율
+left = [(-2,0,0.02),(-1,0,0.07),(-1,-1,0.1),(-1,1,0.01),(0,-2,0.05),(1,-1,0.1),(1,0,0.07),(1,1,0.01),(2,0,0.02),(0,-1,0)]
+right = [(x,-y,z) for x,y,z in left]
+up = [(y,x,z) for x,y,z in left]
+down = [(-y,x,z) for x,y,z in left]
+
+rate = {'left':left, 'right':right, 'up':up, 'down':down}
+
+#나간 모래
 ans = 0
 
-def cnt(sx,sy,direction):
-    global ans
-    
-    if sy < 0:
-        return
-    
-    total = 0
-    for dx,dy,z in direction:
-        nx = sx + dx
-        ny = sy + dy
-        # a일때
-        if z == 0:
-            sand = board[sx][sy] - total
-        else:
-            sand = int(board[sx][sy] * z)
-            total += sand
+
+#모리 흩날림
+def move(cnt,dx,dy,direction):
+    global ans,sx,sy
+    for _ in range(cnt + 1):
+        sx,sy = sx+dx,sy+dy
+        #범위 벗어나면 멈추기
+        if sx < 0 or sy < 0:
+            break
         
-        if 0 <= nx < N and 0 <= ny < N:
-            board[nx][ny] += sand
+        total = 0
+        for dx,dy,r in rate[direction]:
+            nx = sx + dx
+            ny = sy + dy
+            #a
+            if r == 0:
+                s = sand[sx][sy] - total
+            #그외 퍼지는 모래
+            else:
+                s = int(sand[sx][sy]*r)
+            
+            if 0 <= nx < N and 0<= ny < N:
+                sand[nx][ny] += s
+            else:
+                ans += s
+            total += s
+            
         
-        else:
-            ans += sand
 
 
+for i in range(N):
+    if i % 2 == 0:
+        move(i,0,-1,'left')
+        move(i,1,0,'down')
+    else:
+        move(i,0,1,'right')
+        move(i,-1,0,'up')
 
-
-#좌하우상
-dx = [0,1,0,-1]
-dy = [-1,0,1,0]
-
-#방향별 모래 비율
-left = [(1,1,0.01),(-1,1,0.01),(1,0,0.07),(-1,0,0.07),(-1,-1,0.1),(1,-1,0.1),(2,0,0.02),(-2,0,0.02),(0,-2,0.05),(0,-1,0)]
-right =[(x,-y,z) for x,y,z in left]
-down = [(-y,x,z) for x,y,z in left]
-up = [(y,x,z) for x,y,z in left]
-
-#토네이도 회전 순서
-dict = {0: left, 1: down, 2: right, 3:up}
-t = 0
-for i in range(2*N-1):
-    d = i % 4
-    if d == 0 or d == 2 :
-        t += 1
-    for _ in range(t):
-        nx = sx + dx[d]
-        ny = sy + dy[d]
-        cnt(nx,ny,dict[d])
-        sx,sy = nx,ny
-        
-print(ans)
+print(ans)    
