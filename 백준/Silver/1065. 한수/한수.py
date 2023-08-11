9c61b2955cea3c299424fe3bbd71c9b70a1003c2
Hoonies <seunghun311@naver.com>
2023-08-11 14:11:48 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+ans = 0
+for i in range(1,N+1):
+    a = list(map(int,str(i)))
+    if len(a) < 2:
+        ans += 1
+    else:
+        gap = a[1]-a[0]
+        cnt = 0
+        for j in range(1,len(a)):
+            if a[j]-a[j-1] == gap:
+                cnt += 1
+        if cnt == len(a)-1:
+            ans+=1 
+
+print(ans)
