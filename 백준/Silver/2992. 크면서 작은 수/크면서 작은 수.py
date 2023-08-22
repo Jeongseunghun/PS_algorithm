c3b4e91216b181674950433a741917c73954f8e3
Hoonies <seunghun311@naver.com>
2023-08-22 17:02:13 +0900

    [Silver III] Title: 크면서 작은 수, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
@@ -0,0 +1,29 @@
+x = input()
+
+length = len(x)
+min_num = "999999"
+num = ""
+used = [False] * length
+
+def dfs(cnt):
+    global min_num,num
+ 
+    if cnt == length:
+        if x < num:
+            min_num = min(min_num,num)
+        return
+
+    for i in range(length):
+        if used[i] == True:
+            continue
+        used[i] = True
+        num += x[i]
+        dfs(cnt+1)
+        used[i] = False
+        num = num[:-1]
+
+dfs(0)
+
+if min_num == "999999":
+    min_num = 0
+print(min_num)
