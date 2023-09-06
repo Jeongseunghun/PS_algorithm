b198e62f30c5c31a4a40013ec058acfa6ede4c20
Hoonies <seunghun311@naver.com>
2023-09-06 16:52:05 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1592 ms, Memory: 277952 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,17 +1,14 @@
 from collections import deque
 
 N,M = map(int,input().split())
-loc = [list(map(int,input())) for _ in range(N)]
+board = [list(map(int,input())) for _ in range(N)]
 
-#벽 부순적 있으면 z=1, 없으면 z=0
-visited = [[[0,0] for _ in range(M)] for _ in range(N)]
-
-#상하좌우
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
 def bfs():
     q = deque()
+    visited = [[[0,0] for _ in range(M)] for _ in range(N)]
     q.append((0,0,0))
     visited[0][0][0] = 1
     while q:
@@ -19,20 +16,18 @@ def bfs():
         if x == N-1 and y == M-1:
             return visited[x][y][z]
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx<0 or ny<0 or nx>=N or ny>=M:
-                continue
-            
-            #벽 부순적없고 벽밖에 없을 때
-            if loc[nx][ny] == 1 and z == 0:
-                visited[nx][ny][1] = visited[x][y][0] + 1
-                q.append((nx,ny,1))
-                
-            elif loc[nx][ny] == 0 and visited[nx][ny][z] == 0:
-                visited[nx][ny][z] = visited[x][y][z] + 1
-                q.append((nx,ny,z))
-    
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<= nx < N and 0<= ny < M:
+                #벽이 아니고, 방문한 적 없을 때
+                if board[nx][ny] == 0 and visited[nx][ny][z] == 0:
+                    q.append((nx,ny,z))
+                    visited[nx][ny][z] = visited[x][y][z] + 1
+                #벽이고, 부순 적 없고, 방문한 적 없을때
+                elif board[nx][ny] == 1 and z == 0 and visited[nx][ny][1] == 0:
+                    q.append((nx,ny,1))
+                    visited[nx][ny][1] = visited[x][y][0] + 1 
+              
     return -1
-            
-print(bfs())
+
+print((bfs()))
