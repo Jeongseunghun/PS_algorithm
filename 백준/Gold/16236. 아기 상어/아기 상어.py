f0914df11fde46471abc475217f3d78b5e28a411
Hoonies <seunghun311@naver.com>
2023-09-11 17:09:54 +0900

    [Gold III] Title: 아기 상어, Time: 192 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,66 +1,61 @@
 from collections import deque
-N = int(input())
 
+N = int(input())
+board = [list(map(int,input().split())) for _ in range(N)]
+#아기상어 초기 위치
+for i in range(N):
+    for j in range(N):
+        if board[i][j] == 9:
+            sx,sy = i,j
 #상하좌우
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-space = [list(map(int,input().split())) for _ in range(N)]
 
-s_size = 2
-s_eat = 0
-
-for i in range(N):
-    for j in range(N):
-        if space[i][j] == 9:
-            space[i][j] = 0
-            shark_x, shark_y = i, j
-
-
-def bfs(sx,sy):
-    global s_size
+def bfs():
+    global sx,sy,shark_size
     q = deque()
-    q.append([sx,sy])
-
-    visited = [[False for _ in range(N)] for _ in range(N)]
+    q.append((sx,sy))
+    visited = [[0 for _ in range(N)] for _ in range(N)]
     dis = [[0 for _ in range(N)] for _ in range(N)]
-    can_eat_fish = []
-    
+    lst = []
     while q:
         x,y = q.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+        visited[x][y] = 1
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
             if 0 <= nx < N and 0 <= ny < N:
-                if space[nx][ny] <= s_size and not visited[nx][ny]:
-                    visited[nx][ny] = True
+                #아기 상어 크기보다 물고기 크기가 작거나 같아야 이동 가능
+                if board[nx][ny] <= shark_size and visited[nx][ny] == 0:
+                    q.append((nx,ny))
+                    visited[nx][ny] = 1
                     dis[nx][ny] = dis[x][y] + 1
-                    q.append([nx,ny])
-                    
-                    if space[nx][ny] < s_size and space[nx][ny] != 0:
-                        can_eat_fish.append([nx,ny,dis[nx][ny]])
-    
-    can_eat_fish.sort(key= lambda x : (x[2],x[0],x[1]))
-    return can_eat_fish
+                    #상어 크기보다 작으면
+                    if board[nx][ny] < shark_size and board[nx][ny] != 0:
+                        lst.append([dis[nx][ny],nx,ny])
+    lst.sort()
+    return lst
 
+def shark_move():
+    global sx,sy,t,cnt
+    dis = bfs()
+    if not dis:
+        print(t)
+        exit()
+    distance, x, y = dis[0]
+    #상어 위치 갱신
+    board[sx][sy] = 0
+    sx,sy = x,y
+    cnt+=1
+    return distance
 
-#물고기 잡아먹는 누적 시간
-ans = 0
-
+#시간
+t = 0
+#물고기 잡아먹은 횟수
+cnt = 0
+shark_size = 2
 while True:
-    fishlst = bfs(shark_x,shark_y)
-    #아무 것도 못먹으면 종료     
-    if len(fishlst) == 0:
-        print(ans)
-        exit(0)
-        
-    shark_x,shark_y,move_time = fishlst[0]
-    
-    s_eat+=1
-    
-    #아기 상어 크기
-    if s_size == s_eat:
-        s_eat = 0
-        s_size += 1
-        
-    space[shark_x][shark_y] = 0
-    ans += move_time
-    
+    t += shark_move()
+    if cnt == shark_size:
+        shark_size += 1
+        cnt = 0
