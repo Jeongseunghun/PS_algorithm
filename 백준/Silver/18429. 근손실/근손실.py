126cee51cea5c14b782aeefffea47b81e99228f6
Hoonies <seunghun311@naver.com>
2023-09-13 13:44:38 +0900

    [Silver III] Title: 근손실, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.py
+++ 백준/Silver/18429. 근손실/근손실.py
@@ -1,23 +1,20 @@
-from itertools import permutations
-
 N,K = map(int,input().split())
 kit = list(map(int,input().split()))
-
-tmp = []
-for i in permutations(kit,len(kit)):
-    tmp.append(i)
-
+chk = [0] * N
 cnt = 0
-for case in tmp:
-    strength = 500
-    flag = True
-    for i in case:
-        strength -= K
-        strength += i
-        if strength < 500:
-            flag = False
-            break
-    if flag == True:
-        cnt+=1
 
+def dfs(depth,t):
+    global cnt
+    if t < 500:
+        return
+    if depth >= N:
+        cnt+=1
+        return
+    for i in range(N):
+        if chk[i] == 0:
+            chk[i] = 1
+            dfs(depth+1, t+kit[i]-K)
+            chk[i] = 0
+
+dfs(0,500)
 print(cnt)
