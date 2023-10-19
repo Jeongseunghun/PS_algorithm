db48e42eebce669c9e50920839c8fa80b727d2cb
Hoonies <seunghun311@naver.com>
2023-10-19 17:10:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 68 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -10,11 +10,4 @@ while len(lst) > 0:
     lst.rotate(-(K-1))
     ans.append(lst.popleft())
 
-res = '<'
-for i in range(N-1):
-    res+=str(ans[i])
-    res+=', '
-res += str(ans[-1])
-res += '>'
-
-print(res)
+print(str(ans).replace('[','<').replace(']','>'))
