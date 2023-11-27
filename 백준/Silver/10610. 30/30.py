ca0735d16d4fd552a95f29cceec76132a6d444e5
Hoonies <seunghun311@naver.com>
2023-11-27 14:41:24 +0900

    [Silver IV] Title: 30, Time: 76 ms, Memory: 32252 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,13 @@
+N = input()
+N = sorted(N,reverse=True)
+
+s = 0
+if '0' not in N:
+    print(-1)
+else:
+    for i in N:
+        s+=int(i)
+    if s % 3 != 0:
+        print(-1)
+    else:
+        print(''.join(N))
