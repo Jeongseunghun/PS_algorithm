271d54eb9b9f1115c6ec82539fe9cddf3f9b1226
Hoonies <seunghun311@naver.com>
2023-12-20 19:52:47 +0900

    [Gold IV] Title: 전화번호 목록, Time: 280 ms, Memory: 116692 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,19 +1,10 @@
-import sys
-
-input = sys.stdin.readline
-
 t = int(input())
 for _ in range(t):
     flag = 'YES'
-    n = int(input().strip())
-    lst = []
-    for i in range(n):
-        num = input().strip()
-        lst.append(num)
+    n = int(input())
+    lst = [input() for _ in range(n)]
     lst.sort()
     for i in range(n-1):
-        for j in range(i+1,n):
-            if lst[i] == lst[j][:len(lst[i])]:
-                flag = 'NO'
+        if lst[i] == lst[i+1][:len(lst[i])]:
+            flag = 'NO'
     print(flag)
-            
