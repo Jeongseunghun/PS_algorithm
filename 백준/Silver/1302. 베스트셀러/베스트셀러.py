43a537d5792e79e17d9f5263666d658fa75b68fc
Hoonies <seunghun311@naver.com>
2023-12-22 18:57:56 +0900

    [Silver IV] Title: 베스트셀러, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,12 @@
+N = int(input())
+dict = {}
+for _ in range(N):
+    book = input()
+    if book not in dict:
+        dict[book] = 1
+    else:
+        dict[book] +=1
+
+dict = sorted(dict,key = lambda x : (-dict[x],x))
+
+print(dict[0])
