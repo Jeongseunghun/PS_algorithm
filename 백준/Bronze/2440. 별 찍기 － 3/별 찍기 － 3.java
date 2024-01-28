ab05888671b2c0d046cbdbdf98e0f99e2193bd0c
Hoonies <seunghun311@naver.com>
2024-01-28 15:25:20 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 340 ms, Memory: 18164 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = 0 ; i <= N ; i++){
+            for(int j = N; j-i > 0 ; j--){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        
+    }
+    
+}
