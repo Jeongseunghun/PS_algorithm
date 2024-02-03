497bcf29fc035f879bce6ebab1ce1219b3512e14
Hoonies <seunghun311@naver.com>
2024-02-03 12:40:16 +0900

    [Silver V] Title: 식당 입구 대기 줄, Time: 224 ms, Memory: 116176 KB -BaekjoonHub

--- 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.py
+++ 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.py
@@ -1,19 +1,20 @@
 from collections import deque
 
-n = int(input())
-m = [0,0]
-s = []
-for _ in range(n):
+N = int(input())
+m = 0
+n = 0
+q = deque()
+for _ in range(N):
     lst = list(map(int,input().split()))
     if lst[0] == 1:
-        s.append(lst[1])
+        q.append(lst[1])
+        if m < len(q):
+            m = len(q)
+            n = q[-1]
+        if m == len(q):
+            if q[-1] < n:
+                n = q[-1]
     else:
-        s.pop(0)
-    
-    if m[0] < len(s):
-        m[0] = len(s)
-        m[1] = s[-1]
-    elif m[0] == len(s):
-        m[1] = min(m[1],s[-1])
+        q.popleft()
 
-print(m[0],m[1])
+print(m,n)
