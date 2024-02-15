3bae71798630efb185949b19272d9109222ff996
Hoonies <seunghun311@naver.com>
2024-02-15 10:34:44 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 4440 ms, Memory: 56384 KB -BaekjoonHub

--- 백준/Gold IV/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold IV/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,29 +1,36 @@
-#Prim
-import heapq
+#Kruskal
 
 V,E = map(int,input().split())
-board = [[] for _ in range(V+1)]
 
-visited = [0 for _ in range(V+1)]
-h = [[0,1]]
-ans = 0
-cnt = 0
+#root를 저장
+root = [i for i in range(V+1)]
 
+lst = []
 for _ in range(E):
-    a,b,c = map(int,input().split())
-    board[a].append([c,b])
-    board[b].append([c,a])
+    lst.append(list(map(int,input().split())))
 
-while h:
-    if cnt == V:
-        break
+#가중치 기준 정렬
+lst.sort(key=lambda x : x[2])
 
-    c,b = heapq.heappop(h)
-    if visited[b] == 0:
-        visited[b] = 1
-        ans+=c
-        cnt+=1
-        for i in board[b]:
-            heapq.heappush(h,i)
-    
+
+def find(x):
+    if x != root[x]:
+        root[x] = find(root[x])
+    return root[x]
+
+ans = 0
+#간선 리스트 순회
+for a,b,c in lst:
+    #시작 정점 a의 root 찾기
+    sRoot = find(a)
+    #끝 정점 b의 root 찾기
+    eRoot = find(b)
+    #시작 정점과 끝 정점의 root가 같지 않아야 사이클 형성 X
+    if sRoot != eRoot:
+        if sRoot > eRoot:
+            root[sRoot] = eRoot
+        else:
+            root[eRoot] = sRoot
+        ans += c
+
 print(ans)
