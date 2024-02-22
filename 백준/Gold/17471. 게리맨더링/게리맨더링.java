a2305955e70a4bfd4a5ce3ca40aeb95c61cf00bb
Hoonies <seunghun311@naver.com>
2024-02-22 17:48:22 +0900

    [Gold IV] Title: 게리맨더링, Time: 156 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,9 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.StringTokenizer;
 import java.util.List;
 import java.util.Arrays;
 
@@ -13,20 +17,24 @@ public class Main {
     static List<Integer>[] area;
     static int res = Integer.MAX_VALUE;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
         lst = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            lst[i] = scanner.nextInt();
+            lst[i] = Integer.parseInt(st.nextToken());
         }
 
         area = new ArrayList[N + 1];
+        
         for (int i = 1; i <= N; i++) {
+        	st = new StringTokenizer(br.readLine());
             area[i] = new ArrayList<>();
-            int count = scanner.nextInt();
+            int count = Integer.parseInt(st.nextToken());
             for (int j = 0; j < count; j++) {
-                area[i].add(scanner.nextInt());
+                area[i].add(Integer.parseInt(st.nextToken()));
             }
         }
 
