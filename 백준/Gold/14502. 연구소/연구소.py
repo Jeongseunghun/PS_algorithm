c8c8777601ef560dc755c25c3fcc19ff33ff15ab
Hoonies <seunghun311@naver.com>
2024-02-29 13:35:41 +0900

    [Gold IV] Title: 연구소, Time: 740 ms, Memory: 119156 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,61 +1,64 @@
-from collections import deque
 import copy
-import sys
-input = sys.stdin.readline
+from itertools import combinations
+from collections import deque
+
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+
+def bfs(tmp_board):
+    q = deque()
+    visited = [[0 for _ in range(M)] for _ in range(N)]
+    for i in virus_lst:
+        q.append(i)
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M:
+                if visited[nx][ny] == 0 and tmp_board[nx][ny] == 0:
+                    q.append((nx,ny))
+                    visited[nx][ny] = 1
+                    tmp_board[nx][ny] = 2
+
+    res = 0
+    for i in range(N):
+        for j in range(M):
+            if tmp_board[i][j] == 0:
+                res+=1
+
+    return res
+
 
 N,M = map(int,input().split())
-RS = [ list(map(int,input().split())) for _ in range(N) ]
+# 0: 빈칸, 1: 벽, 2: 바이러스
+board = [list(map(int,input().split())) for _ in range(N)]
 
-visited= [[0 for _ in range(M)] for _ in range(N)]
+#바이러스 위치
+virus_lst = []
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 2:
+            virus_lst.append((i,j))
 
-#상하좌우
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+wall = []
+for i in range(N):
+    for j in range(M):
+        if board[i][j] != 1 and board[i][j] != 2:
+            wall.append((i,j))
+#벽 좌표 후보
+wall_candi = []
+for i in combinations(wall,3):
+    wall_candi.append(i)
 
+ans = 0
+for i in wall_candi:
+    tmp_board = copy.deepcopy(board)
+    #벽 세우기
+    for j in i:
+        tmp_board[j[0]][j[1]] = 1
 
-def bfs():
-    q = deque()
-    tmp_RS = copy.deepcopy(RS)
-    
-    #큐에 바이러스 위치 넣기
-    for i in range(N):
-        for j in range(M):
-            if tmp_RS[i][j] == 2:
-                q.append((i,j))
-    
-    while q:
-        x,y= q.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            
-            if 0<=nx<N and 0<=ny<M:
-                if tmp_RS[nx][ny] == 0:
-                    tmp_RS[nx][ny] = 2
-                    q.append((nx,ny))
-                    
-    global answer
-    cnt = 0
-    
-    for i in range(N):
-        cnt+=tmp_RS[i].count(0)
-    
-    answer = max(cnt,answer)
+    res = bfs(tmp_board)
+    ans = max(ans,res)
 
-def wall(cnt):
-    if cnt == 3:
-        bfs()
-        return
-    for i in range(N):
-        for j in range(M):
-            if RS[i][j] == 0:
-                RS[i][j] = 1
-                wall(cnt+1)
-                RS[i][j] = 0
-                
-
-            
-answer = 0
-wall(0)
-print(answer)      
-        
+print(ans)
