f1599536a7ef1292474f020598688b6d36ff38fe
Hoonies <seunghun311@naver.com>
2024-04-14 14:54:08 +0900

    [Gold IV] Title: 여행 가자, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -0,0 +1,34 @@
+def find(x):
+    if x != parents[x]:
+        parents[x] = find(parents[x])
+    return parents[x]
+
+
+def union(x,y):
+    x = find(x)
+    y = find(y)
+    if x < y:
+        parents[y] = x
+    else:
+        parents[x] = y
+
+
+N = int(input())
+M = int(input())
+board = [list(map(int,input().split())) for _ in range(N)]
+city = list(map(int,input().split()))
+parents = [i for i in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if board[i][j] == 1:
+            union(i,j)
+parents = [-1] + parents
+s = parents[city[0]]
+for i in range(1,M):
+    if parents[city[i]] != s:
+        print("NO")
+        break
+    
+else:
+    print("YES")
