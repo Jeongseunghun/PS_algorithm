d7000c21323769200bb59237e91ff376a6271d7a
Hoonies <seunghun311@naver.com>
2024-04-19 09:21:47 +0900

    [Gold V] Title: 용액 합성하기, Time: 72 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.py
@@ -0,0 +1,25 @@
+N = int(input())
+lst = list(map(int,input().split()))
+
+s=0
+e=N-1
+ans = 200000000
+while s<e:
+    chemi = lst[s] + lst[e]
+    if abs(ans) >= abs(chemi):
+        ans = chemi
+        if chemi > 0:
+            e-=1
+        elif chemi < 0:
+            s+=1
+        else:
+            break
+    else:
+        if chemi > 0:
+            e-=1
+        elif chemi < 0:
+            s+=1
+        else:
+            break
+
+print(ans)
