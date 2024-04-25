28bea5b8da6bfa190579ab9e0233731d481de1cc
Hoonies <seunghun311@naver.com>
2024-04-25 10:53:48 +0900

    [Silver III] Title: 게임, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,17 @@
+X,Y = map(int,input().split())
+Z = (100 * Y) // X
+
+if Z >= 99:
+    print(-1)
+else:
+    ans = 0
+    s = 0
+    e = 1000000000
+    while s<=e:
+        m = (s+e) // 2
+        if (Y+m) * 100 // (X+m) > Z:
+            ans = m
+            e = m-1
+        else:
+            s = m+1
+    print(ans)
