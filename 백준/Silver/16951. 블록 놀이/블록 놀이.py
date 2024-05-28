737677646a9c0fa2b0990ee3f1c90e07b55ff35e
Hoonies <seunghun311@naver.com>
2024-05-28 18:36:20 +0900

    [Silver IV] Title: 블록 놀이, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/16951. 블록 놀이/블록 놀이.py
@@ -0,0 +1,25 @@
+def chk(i):
+    cnt = 0 
+    for j in range(N):
+        if j < i:
+            if lst[j] != (lst[i] - (i-j)*K):
+                cnt+=1
+        elif j > i:
+            if lst[j] != (lst[i] + (j-i)*K):
+                cnt+=1
+    return cnt
+
+N,K = map(int,input().split())
+lst = list(map(int,input().split()))
+
+cnt = 1001
+if N == 1:
+    cnt = 0
+else:
+    for i in range(N):
+        if lst[i] > i*K:
+            tmp = chk(i)
+            cnt = min(cnt,tmp)
+            
+print(cnt)
+                
