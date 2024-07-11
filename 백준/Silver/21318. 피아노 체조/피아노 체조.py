2f36ca84d6165b2247b0c4399a0cc6b42c46e389
Hoonies <seunghun311@naver.com>
2024-07-11 21:20:40 +0900

    [Silver I] Title: 피아노 체조, Time: 3244 ms, Memory: 128040 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.py
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.py
@@ -3,14 +3,13 @@ lst = list(map(int,input().split()))
 
 dp = [0] * N
 
-Q = int(input())
-
 for i in range(1,N):
     if lst[i-1] > lst[i]:
         dp[i] = dp[i-1] + 1
     else:
         dp[i] = dp[i-1]
 
+Q = int(input())
 for _ in range(Q):
     x,y = map(int,input().split())
     ans = dp[y-1] - dp[x-1]
