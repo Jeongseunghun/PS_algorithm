6ad38ff923b6c7730dcb064af77984dbb54b7c3d
Hoonies <seunghun311@naver.com>
2024-07-16 20:39:38 +0900

    [Silver I] Title: 후보 추천하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -0,0 +1,34 @@
+N = int(input())
+num = int(input())
+students = list(map(int,input().split()))
+
+paging = {}
+for i in range(num):
+    #학생이 없으면
+    if students[i] not in paging:
+        #비어있는 사진틀이 있으면
+        if len(paging)< N:
+            paging[students[i]] = [1,i]
+        #비어있는 사진틀이 없으면
+        else:
+            lst = sorted(paging.items(), key = lambda x : (x[1][0],x[1][1]))
+            tmp = lst[0][0]
+            del paging[tmp]
+            paging[students[i]] = [1,i]
+    #학생이 있으면
+    else:
+        paging[students[i]][0] += 1        
+
+ 
+ans = list(sorted(paging.keys()))
+
+for i in ans:
+    print(i,end =" ")        
+        
+        
+            
+            
+        
+        
+        
+    
