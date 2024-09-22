ed83a6970dc44a306086fac407989c7080262bf5
Hoonies <seunghun311@naver.com>
2024-09-22 17:56:33 +0900

    [Silver I] Title: 금민수의 개수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1527. 금민수의 개수/금민수의 개수.py
@@ -0,0 +1,29 @@
+def dfs(i,str):
+    global ans
+    if i == len(str):
+        if int(str) >= A and int(str) <= B:
+            ans+=1
+            return
+        return
+    for l in lst:
+        dfs(i,str+l)
+    
+    return
+
+A,B = map(int,input().split())
+
+lst = ['4','7']
+
+#A의 자릿수
+ad = len(str(A))
+#B의 자릿수
+bd = len(str(B))
+
+ans = 0
+
+for i in range(ad,bd+1):
+    dfs(i,'')
+
+        
+print(ans)
+    
