8b4d22793e3e2e7f92776e84a322e6a2cd33df9d
Hoonies <seunghun311@naver.com>
2024-10-05 21:35:42 +0900

    [Gold V] Title: 용액, Time: 120 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -3,20 +3,15 @@ lst = list(map(int,input().split()))
 
 s = 0
 e = N-1
-x,y = 0,0
-standard = 2000000000
 
-
+ans = 2000000000
+x,y = 0,N-1
 while s<e:
-    chemi = lst[s] + lst[e]
-    if abs(chemi) <= standard:
-        x = lst[s]
-        y = lst[e]
-        standard = abs(chemi)
-        
-    if chemi <= 0:
+    if abs(lst[s] + lst[e]) < ans:
+        ans = abs(lst[s] + lst[e])
+        x,y = s,e
+    if lst[s] + lst[e] <= 0:
         s+=1
     else:
         e-=1
-
-print(x,y)
+print(lst[x],lst[y])
