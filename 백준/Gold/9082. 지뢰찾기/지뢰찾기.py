ca745753473dd94af01fc4b9f9cd3540d2530bc3
Hoonies <seunghun311@naver.com>
2024-10-21 08:33:07 +0900

    [Gold IV] Title: 지뢰찾기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9082. 지뢰찾기/지뢰찾기.py
@@ -0,0 +1,31 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    board = list(input())
+    for i in range(N):
+        board[i] = int(board[i])
+    mine = input()
+    ans = 0
+    for i in range(N):
+        #첫 번째 지뢰
+        if i == 0:
+            if board[i] != 0 and board[i+1] != 0:
+                board[i] -= 1
+                board[i+1] -= 1
+                ans+=1
+        #마지막 지뢰
+        elif i == N-1:
+            if board[i] != 0 and board[i-1] != 0:
+                board[i] -= 1
+                board[i-1] -= 1
+                ans+=1
+        #중간 지뢰
+        else:
+            if board[i-1] != 0 and board[i] != 0 and board[i+1] != 0:
+                board[i-1] -= 1
+                board[i] -= 1
+                board[i+1] -= 1
+                ans += 1
+    print(ans)
+            
+            
