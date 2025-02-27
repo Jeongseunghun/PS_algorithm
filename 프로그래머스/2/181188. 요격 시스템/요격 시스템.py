7a32114118ede3a54845e06fb1c943d0596fda42
Hoonies <seunghun311@naver.com>
2025-02-27 09:47:59 +0900

    [level 2] Title: 요격 시스템, Time: 378.20 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,11 +1,10 @@
 def solution(targets):
-    targets.sort(key = lambda x : (x[1]))
-    standard = targets[0][1]-1
+    targets.sort(key = lambda x : x[1])
+    standard = targets[0][1]-0.5
     ans = 1
     for t in range(1,len(targets)):
         if standard < targets[t][0]:
             ans += 1
-            standard = targets[t][1] - 1
-            print(standard)
+            standard = targets[t][1] - 0.5
             
     return ans
