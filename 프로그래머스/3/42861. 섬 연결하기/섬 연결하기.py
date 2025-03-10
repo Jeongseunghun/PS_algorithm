4abbac8df38d9931f513a3bada3d9a8f773f725e
Hoonies <seunghun311@naver.com>
2025-03-10 20:32:08 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,17 @@
+def solution(n, costs):
+    answer = 0
+    
+    costs.sort(key = lambda x : x[2])
+    link = set()
+    link.add(costs[0][0])
+    
+    while len(link) != n:
+        for s,e,c in costs:
+            if s in link and e in link:
+                continue
+            if s in link or e in link:
+                link.update([s,e])
+                answer += c
+                break
+
+    return answer
