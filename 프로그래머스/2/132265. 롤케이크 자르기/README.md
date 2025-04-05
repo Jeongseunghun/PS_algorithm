1c6aa4878a49f4bacd10f83afa4d39c3689cde28
Hoonies <seunghun311@naver.com>
2025-04-05 12:35:24 +0900

    [level 2] Title: 롤케이크 자르기, Time: 677.41 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
 
 ### 성능 요약
 
-메모리: 50.6 MB, 시간: 744.40 ms
+메모리: 50.6 MB, 시간: 677.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 16일 19:44:16
+2025년 04월 05일 12:35:19
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,15 +1,17 @@
 from collections import Counter
 
 def solution(topping):
-    dic = Counter(topping)
-    set_dic = set()
-    res = 0
+    answer = 0
+    dict = Counter(topping)
+    
+    s = set()
     for i in topping:
-        dic[i] -= 1
-        set_dic.add(i)
-        if dic[i] == 0:
-            dic.pop(i)
-        if len(dic) == len(set_dic):
-            res+=1
-
-    return res
+        dict[i] -= 1
+        if dict[i] == 0:
+            del dict[i]
+        s.add(i)
+        if len(dict) == len(s):
+            answer+=1
+        
+    
+    return answer
