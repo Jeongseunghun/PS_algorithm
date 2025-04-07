0a5168aeb689fe4657581a0a75bccbfe8ab265d7
Hoonies <seunghun311@naver.com>
2025-04-07 09:35:47 +0900

    [level 3] Title: 야근 지수, Time: 821.74 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,17 +1,18 @@
 import heapq
 
 def solution(n, works):
-    if sum(works) <= n:
-        return 0
-    
-    ans = 0
+    answer = 0
     works = [-i for i in works]
+
     heapq.heapify(works)
-    while n > 0:
-        tmp = heapq.heappop(works)
-        heapq.heappush(works,tmp+1)
-        n-=1
+    for i in range(n):
+        a = heapq.heappop(works)
+        if a < 0:
+            heapq.heappush(works,a+1)
+        else:
+            heapq.heappush(works,0)
     
     for i in works:
-        ans+=i**2
-    return ans
+        answer += i**2
+        
+    return answer
