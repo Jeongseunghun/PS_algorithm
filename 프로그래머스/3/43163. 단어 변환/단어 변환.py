028ed67e61f90e433ed635ddaec868ff85feca1e
Hoonies <seunghun311@naver.com>
2025-04-14 14:49:24 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    
+    visited = set()
+    
+    def convert(w1,w2):
+        cnt = 0
+        for a,b in zip(w1,w2):
+            if a != b:
+                cnt+=1
+        if cnt == 1:
+            return True
+    
+    q = deque()
+    q.append([begin,0])
+    
+    while q:
+        word,cnt = q.popleft()
+        if word == target:
+            return cnt
+        
+        for w in words:
+            if w not in visited and convert(word,w):
+                visited.add(w)
+                q.append([w,cnt+1])
+    
+    return 0
